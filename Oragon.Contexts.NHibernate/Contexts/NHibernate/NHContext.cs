c14160675d12cbcc9b089720c4d269590428c542
Luiz Carlos Faria <luizcarlosfaria@gmail.com>
2018-07-23 11:18:18 -0300

    Revisão do NHContext

--- Oragon.Contexts.NHibernate/Contexts/NHibernate/NHContext.cs
+++ Oragon.Contexts.NHibernate/Contexts/NHibernate/NHContext.cs
@@ -3,107 +3,108 @@ using NH = NHibernate;
 
 namespace Oragon.Contexts.NHibernate
 {
-	public class NHContext : AbstractContext<NHContextAttribute>
-	{
-		#region Public Constructors
+    public class NHContext : AbstractContext<NHContextAttribute>
+    {
+        #region Public Constructors
 
-		public NHContext(NHContextAttribute contextAttribute, NH.IInterceptor interceptor, Stack<AbstractContext<NHContextAttribute>> contextStack)
-			: base(contextAttribute, contextStack)
-		{
-			this.Session = this.BuildSession(interceptor);
-			this.Transaction = this.BuildTransaction();
-		}
+        public NHContext(NHContextAttribute contextAttribute, NH.IInterceptor interceptor, Stack<AbstractContext<NHContextAttribute>> contextStack)
+            : base(contextAttribute, contextStack)
+        {
+            this.Session = this.BuildSession(interceptor);
+            this.Transaction = this.BuildTransaction();
+        }
 
-		#endregion Public Constructors
+        #endregion Public Constructors
 
-		#region Public Properties
+        #region Public Properties
 
-		public NH.ISession Session { get; private set; }
+        public NH.ISession Session { get; private set; }
 
-		#endregion Public Properties
+        #endregion Public Properties
 
-		#region Protected Properties
+        #region Protected Properties
 
-		protected bool IsTransactional
-		{
-			get
-			{
-				return (this.ContextAttribute.IsTransactional.HasValue && this.ContextAttribute.IsTransactional.Value);
-			}
-		}
+        protected bool IsTransactional
+        {
+            get
+            {
+                return (this.ContextAttribute.IsTransactional.HasValue && this.ContextAttribute.IsTransactional.Value);
+            }
+        }
 
-		#endregion Protected Properties
+        #endregion Protected Properties
 
-		#region Private Properties
+        #region Private Properties
 
-		private NH.ITransaction Transaction { get; set; }
+        private NH.ITransaction Transaction { get; set; }
 
-		#endregion Private Properties
+        #endregion Private Properties
 
-		#region Public Methods
+        #region Public Methods
 
-		public bool Complete()
-		{
-			bool returnValue = (this.IsTransactional && this.Transaction != null);
-			if (returnValue)
-				this.Transaction.Commit();
-			return returnValue;
-		}
+        public bool Complete()
+        {
+            bool returnValue = (this.IsTransactional && this.Transaction != null);
+            if (returnValue)
+                this.Transaction.Commit();
+            return returnValue;
+        }
 
-		#endregion Public Methods
+        #endregion Public Methods
 
-		#region Private Methods
+        #region Private Methods
 
-		/// <summary>
-		/// Realiza o Build de um ISession
-		/// </summary>
-		/// <param name="interceptor"></param>
-		/// <returns></returns>
-		private NH.ISession BuildSession(NH.IInterceptor interceptor)
-		{
-			NH.ISessionFactory sessionFactory = this.ContextAttribute.SessionFactoryBuilder.BuildSessionFactory();
-			NH.ISession session = interceptor != null ? sessionFactory.OpenSession(interceptor) : sessionFactory.OpenSession();
+        /// <summary>
+        /// Realiza o Build de um ISession
+        /// </summary>
+        /// <param name="interceptor"></param>
+        /// <returns></returns>
+        private NH.ISession BuildSession(NH.IInterceptor interceptor)
+        {
+            NH.ISessionFactory sessionFactory = this.ContextAttribute.SessionFactoryBuilder.BuildSessionFactory();
+            NH.ISessionBuilder sessionBuilder = sessionFactory.WithOptions();
+            if (interceptor != null)
+            {
+                sessionBuilder = sessionBuilder.Interceptor(interceptor);
+            }
+            sessionBuilder = sessionBuilder.FlushMode(this.IsTransactional ? this.ContextAttribute.SessionFactoryBuilder.TransactionFlushMode : this.ContextAttribute.SessionFactoryBuilder.DefaultFlushMode);
+            NH.ISession session = sessionBuilder.OpenSession();
+            return session;
+        }
 
-			if (this.IsTransactional)
-				session.FlushMode = this.ContextAttribute.SessionFactoryBuilder.TransactionFlushMode;
-			else
-				session.FlushMode = this.ContextAttribute.SessionFactoryBuilder.DefaultFlushMode;
-			return session;
-		}
+        private NH.ITransaction BuildTransaction()
+        {
+            NH.ITransaction returnValue = null;
+            if (this.IsTransactional)
+            {
+                returnValue = this.Session.BeginTransaction(this.ContextAttribute.SessionFactoryBuilder.TransactionIsolationLevel);
+            }
+            return returnValue;
+        }
 
-		private NH.ITransaction BuildTransaction()
-		{
-			NH.ITransaction returnValue = null;
-			if (this.IsTransactional)
-			{
-				returnValue = this.Session.BeginTransaction(this.ContextAttribute.SessionFactoryBuilder.TransactionIsolationLevel);
-			}
-			return returnValue;
-		}
+        #endregion Private Methods
 
-		#endregion Private Methods
+        #region Dispose Methods
 
-		#region Dispose Methods
+        protected override void DisposeContext()
+        {
+            //TODO: Adicionado tratamento para a transaction. Na versão anterior não havia sido codificado, no entanto também não há nenhum bug conhecido a respeito da falta desse código.
+            if (this.Transaction != null)
+                this.Transaction.Dispose();
 
-		protected override void DisposeContext()
-		{
-			//TODO: Adicionado tratamento para a transaction. Na versão anterior não havia sido codificado, no entanto também não há nenhum bug conhecido a respeito da falta desse código.
-			if (this.Transaction != null)
-				this.Transaction.Dispose();
+            if (this.Session != null)
+                this.Session.Dispose();
 
-			if (this.Session != null)
-				this.Session.Dispose();
+            base.DisposeContext();
+        }
 
-			base.DisposeContext();
-		}
+        protected override void DisposeFields()
+        {
+            this.Session = null;
+            this.Transaction = null;
+            base.DisposeFields();
+        }
 
-		protected override void DisposeFields()
-		{
-			this.Session = null;
-			this.Transaction = null;
-			base.DisposeFields();
-		}
-
-		#endregion Dispose Methods
-	}
+        #endregion Dispose Methods
+    }
 }
