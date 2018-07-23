17d96d78561973d42b0500405536d7faafcd1b48
Luiz Carlos Faria <luizcarlosfaria@gmail.com>
2018-07-23 19:44:52 -0300

    Basic Crud

--- Examples/EnterpriseLog/Oragon.Architecture.LogEngine.ConsoleApp/Services/TestService.cs
+++ Examples/EnterpriseLog/Oragon.Architecture.LogEngine.ConsoleApp/Services/TestService.cs
@@ -1,7 +1,6 @@
 ﻿using Oragon.Architecture.LogEngine.Business.Entity;
 using Oragon.Architecture.LogEngine.Data.Process;
 using Oragon.Contexts.NHibernate;
-using System;
 
 namespace Oragon.Architecture.LogEngine.ConsoleApp.Services
 {
@@ -15,7 +14,11 @@ namespace Oragon.Architecture.LogEngine.ConsoleApp.Services
         [NHContext("APP_DB_PRINCIPAL_CONTEXT", true)]
         public void Test()
         {
-            this.WriteDataProcess.Save(new Aluno() { Nome = "Luiz Carlos Faria", Descricao = "Tete" });
+            //this.WriteDataProcess.Save(new Aluno() { Nome = "Luiz Carlos Faria", Descricao = "Tete" });
+
+            //this.WriteDataProcess.SaveOrUpdate(new Aluno() { IdAluno=1, Nome = "Luiz Carlos Faria", Descricao = "Teste" });
+
+            //this.WriteDataProcess.Delete(new Aluno() { IdAluno = 1, Nome = "a" });
         }
     }
 }
