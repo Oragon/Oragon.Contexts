c4b9976447fdc778cac45e0f80e845b7d062dee5
Luiz Carlos Faria <luizcarlosfaria@gmail.com>
2018-08-28 01:26:05 -0300

    Adição de projeto de testes

---
+++ tests/Oragon.Context.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Oragon.Context.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
