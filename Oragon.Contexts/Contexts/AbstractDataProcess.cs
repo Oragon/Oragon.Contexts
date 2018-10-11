8afc7e5a98846f41b5ec1567d72f25ebac99f0fa
Luiz Carlos Faria <luizcarlosfaria@gmail.com>
2018-10-10 21:53:51 -0300

    FAKE COMMIT

--- Oragon.Contexts/Contexts/AbstractDataProcess.cs
+++ Oragon.Contexts/Contexts/AbstractDataProcess.cs
@@ -4,29 +4,45 @@ using System.Globalization;
 
 namespace Oragon.Contexts
 {
-	public abstract class AbstractDataProcess<ContextType, AttributeType>: IDisposable
-		where ContextType : AbstractContext<AttributeType>
-		where AttributeType : AbstractContextAttribute
-	{
-		#region Protected Properties
+    public abstract class AbstractDataProcess<ContextType, AttributeType> : IDisposable
+        where ContextType : AbstractContext<AttributeType>
+        where AttributeType : AbstractContextAttribute
+    {
+        private bool disposed;
+        #region Protected Properties
 
-		protected virtual ContextType ObjectContext
-		{
-			get
-			{
-				ContextType returnValue = Spring.Threading.LogicalThreadContext.GetData(this.ObjectContextKey) as ContextType;
-				if (returnValue == null)
-					throw new InvalidOperationException(string.Format(CultureInfo.InvariantCulture, "AbstractDataProcess cannot find context with key '{0}'", this.ObjectContextKey));
-				return returnValue;
-			}
-		}
-
-		[Required]
-		protected string ObjectContextKey { get; set; }
-
-        public void Dispose()
+        protected virtual ContextType ObjectContext
         {
-            throw new NotImplementedException();
+            get
+            {
+                ContextType returnValue = Spring.Threading.LogicalThreadContext.GetData(ObjectContextKey) as ContextType;
+                if (returnValue == null)
+                {
+                    throw new InvalidOperationException(string.Format(CultureInfo.InvariantCulture, "AbstractDataProcess cannot find context with key '{0}'", ObjectContextKey));
+                }
+
+                return returnValue;
+            }
+        }
+
+        [Required]
+        protected string ObjectContextKey { get; set; }
+
+        public virtual void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                /*
+                 ...
+                 */
+                disposed = true;
+            }
         }
 
         #endregion Protected Properties
