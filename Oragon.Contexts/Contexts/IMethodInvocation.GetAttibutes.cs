357c7dd22aafc04cc0f412476305268a31b7f4fb
Luiz Carlos Faria <luizcarlosfaria@gmail.com>
2018-11-07 14:47:42 -0200

    Review way to search attributes

--- Oragon.Contexts/Contexts/IMethodInvocation.GetAttibutes.cs
+++ Oragon.Contexts/Contexts/IMethodInvocation.GetAttibutes.cs
@@ -9,16 +9,17 @@ namespace Oragon.Contexts
 	{
 		#region Public Methods
 
-		public static IEnumerable<T> GetAttibutes<T>(this IMethodInvocation invocation, Func<T, bool> predicate = null)
+		public static T[] GetAttibutes<T>(this IMethodInvocation invocation, Func<T, bool> predicate = null)
 			where T : Attribute
 		{
 			if (predicate == null)
 				predicate = (it => true);
 
-			//Recupera os atributos do método
-			IEnumerable<T> returnValue = invocation.Method.GetCustomAttributes(typeof(T), true)
-			.Cast<T>()
-			.Where(predicate);
+            //Recupera os atributos do método
+            T[] returnValue = invocation.Method.GetCustomAttributes(typeof(T), true)
+            .Cast<T>()
+            .Where(predicate)
+            .ToArray();
 			return returnValue;
 		}
 
