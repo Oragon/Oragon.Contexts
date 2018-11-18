8ee6b865f1687835c788583bd312eb39de1c5c40
Luiz Carlos Faria <luizcarlosfaria@gmail.com>
2018-11-18 06:13:57 -0200

    Addi SQLite and PostgreSQL support

---
+++ Oragon.Contexts.FluentNHibernate/Contexts/NHibernate/FluentNHibernateSessionFactoryBuilderForPostgreSQL.cs
@@ -0,0 +1,21 @@
+﻿using FluentNHibernate.Cfg.Db;
+using System;
+using System.Configuration;
+using System.Globalization;
+using FluentNH = FluentNHibernate;
+using NH = NHibernate;
+
+
+namespace Oragon.Contexts.NHibernate
+{
+    public class FluentNHibernateSessionFactoryBuilderForPostgreSQL : FluentNHibernateSessionFactoryBuilderForAnything<PostgreSQLConfiguration, PostgreSQLConnectionStringBuilder>
+    {
+
+        public override PersistenceConfiguration<PostgreSQLConfiguration, PostgreSQLConnectionStringBuilder> BaseConfiguration
+        {
+            get { return FluentNH.Cfg.Db.PostgreSQLConfiguration.PostgreSQL82; }
+            set { throw new InvalidOperationException("FluentNHibernateSessionFactoryBuilderForPostgreSQL does not support setting of BaseConfiguration"); }
+        }
+
+    }
+}
---
+++ Oragon.Contexts.FluentNHibernate/Contexts/NHibernate/FluentNHibernateSessionFactoryBuilderForSQLite.cs
@@ -0,0 +1,43 @@
+﻿
+using FluentNHibernate.Cfg.Db;
+using System;
+using FluentNH = FluentNHibernate;
+
+namespace Oragon.Contexts.NHibernate
+{
+    public class FluentNHibernateSessionFactoryBuilderForSQLite : FluentNHibernateSessionFactoryBuilderForAnything<SQLiteConfiguration, ConnectionStringBuilder>
+    {
+
+        public bool InMemory { get; set; } = false;
+
+        public string FileName { get; set; }
+
+        public string Password { get; set; }
+
+        public override PersistenceConfiguration<SQLiteConfiguration, ConnectionStringBuilder> BaseConfiguration
+        {
+            get
+            {
+                if (this.InMemory == false && (string.IsNullOrWhiteSpace(this.FileName) || (System.IO.File.Exists(this.FileName) == false)))
+                {
+                    throw new InvalidOperationException("FluentNHibernateSessionFactoryBuilderForSQLite require or InMemory (set property InMemory=true) or set a valid FileName ");
+                }
+
+                if (this.InMemory)
+                {
+                    return FluentNH.Cfg.Db.SQLiteConfiguration.Standard.InMemory();
+                }
+                else if (string.IsNullOrWhiteSpace(this.Password))
+                {
+                    return FluentNH.Cfg.Db.SQLiteConfiguration.Standard.UsingFile(this.FileName);
+                }
+                else
+                {
+                    return FluentNH.Cfg.Db.SQLiteConfiguration.Standard.UsingFileWithPassword(this.FileName, this.Password);
+                }
+            }
+            set { throw new InvalidOperationException("FluentNHibernateSessionFactoryBuilderForSQLite does not support setting of BaseConfiguration"); }
+        }
+
+    }
+}
